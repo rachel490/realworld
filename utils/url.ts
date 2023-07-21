7bf1747434d82bd023966902a79758185b875de6
rachel490 <ccori12@naver.com>
2023-07-21 17:41:29 +0900

    :sparkles: Declare generatUrl util function

--- utils/url.ts
+++ utils/url.ts
@@ -7,3 +7,13 @@ export const generateQueryString = (options: { [key: string]: string | number | 
 
   return params.toString();
 };
+
+export const generateURL = (baseUrl: string, searchParams?: { [key: string]: string }) => {
+  const url = new URL(baseUrl);
+
+  if (searchParams) {
+    Object.entries(searchParams).forEach(([key, value]) => url.searchParams.set(key, value));
+  }
+
+  return url.toString();
+};
