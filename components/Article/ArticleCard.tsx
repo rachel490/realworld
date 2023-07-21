7e892f89fc48189f0684543d0581754b2c03af9f
rachel490 <ccori12@naver.com>
2023-07-21 15:03:58 +0900

    :sparkles: Implement Article Card

---
+++ components/Article/ArticleCard.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import { IArticle } from "@/types/api";
+import { parseDate } from "@/utils/date";
+
+interface IProps {
+  articleData: IArticle;
+}
+
+function ArticleCard({ articleData }: IProps) {
+  const {
+    title,
+    favoritesCount,
+    description,
+    author: { username, image },
+    createdAt,
+    slug,
+  } = articleData;
+
+  return (
+    <div className="article-preview">
+      <div className="article-meta">
+        <Link href={`/profile/${username}`}>
+          <Image src={image} alt="profile" width={32} height={32} />
+        </Link>
+        <div className="info">
+          <Link href="/" className="author">
+            {username}
+          </Link>
+          <span className="date">{parseDate(createdAt)}</span>
+        </div>
+        <button className="btn btn-outline-primary btn-sm pull-xs-right">
+          <i className="ion-heart" />
+          {favoritesCount}
+        </button>
+      </div>
+      <Link href={`/article/${slug}`} className="preview-link">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <span>Read more...</span>
+      </Link>
+    </div>
+  );
+}
+
+export default ArticleCard;
