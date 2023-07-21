e788dfbd84136b2a5b407270fabecaad2302cecb
rachel490 <ccori12@naver.com>
2023-07-21 15:27:04 +0900

    :sparkles: Add tag feed when tag clicked

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,34 @@ import FeedNavbar from "@/components/FeedNavbar/FeedNavbar";
 import TagSide from "@/components/Tag/TagSide";
 import ArticleCard from "@/components/Article/ArticleCard";
 
-async function getData() {
-  const res = await realWorldApi.get<IFeedResponse>(API_URI.article.get.GLOBAL_FEED());
+async function getData(tagName?: string) {
+  const res = await realWorldApi.get<IFeedResponse>(API_URI.article.get.GLOBAL_FEED(tagName));
 
   return res.data;
 }
 
-export default async function Home() {
-  const { articles } = await getData();
+export interface IMainPageProps {
+  searchParams: {
+    tagName: string;
+    feed: "tag";
+  };
+}
+
+export default async function Home({ searchParams }: IMainPageProps) {
+  const { articles } = await getData(searchParams.tagName);
+
+  // NOTE: generate feedList
   const feedList = ["Global Feed"];
+  if (searchParams.feed === "tag" && searchParams.tagName) {
+    feedList.push(searchParams.tagName);
+  }
+
+  const getCurrentFeed = () => {
+    const { tagName, feed } = searchParams;
+    console.log("tagname", tagName, feed);
+    if (feed === "tag" && tagName.length) return tagName;
+    return "Global Feed";
+  };
 
   return (
     <div className="home-page">
@@ -23,7 +42,7 @@ export default async function Home() {
       <div className="container page">
         <div className="row">
           <div className="col-md-9">
-            <FeedNavbar feedList={feedList} currentFeed={feedList[0]} />
+            <FeedNavbar feedList={feedList} currentFeed={getCurrentFeed()} />
             {articles.map(article => (
               <ArticleCard key={article.slug} articleData={article} />
             ))}
