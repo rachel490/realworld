0297f5c2bf58b4f45c2d438b1035b48335bafaa4
rachel490 <ccori12@naver.com>
2023-07-22 20:36:41 +0900

    :sparkles: Implement Follow button

---
+++ components/@Shared/Button/FollowButton/FollowButton.tsx
@@ -0,0 +1,15 @@
+interface IProps {
+  isFollowing: boolean;
+  username: string;
+}
+
+function FollowButton({ isFollowing, username }: IProps) {
+  return (
+    <button className="btn btn-sm btn-outline-secondary">
+      <i className="ion-plus-round" />
+      &nbsp; ${isFollowing ? "Unfollow" : "Follow"} {username}
+    </button>
+  );
+}
+
+export default FollowButton;
