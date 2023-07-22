e7e69ec320f47d3090958a3d2e18caa9dc685a1a
rachel490 <ccori12@naver.com>
2023-07-22 20:57:05 +0900

    :sparkles: Implement Article Details page

---
+++ app/article/[slug]/page.tsx
@@ -0,0 +1,53 @@
+import { articleApi } from "@/api/domain/article";
+import TagList from "@/components/Tag/TagList";
+import CommentList from "@/components/@Shared/Comment/CommentList";
+import ArticleMeta from "@/components/Article/ArticleMeta";
+import CommentForm from "@/components/Comment/CommentForm/CommentForm";
+
+interface IProps {
+  params: {
+    slug: string;
+  };
+}
+
+async function ArticlePage({ params }: IProps) {
+  const { article } = await articleApi.getArticleDetails(params.slug);
+  const { title, body, tagList, slug } = article;
+
+  return (
+    <div className="article-page">
+      <div className="banner">
+        <div className="container">
+          <h1>{title}</h1>
+          <ArticleMeta article={article} />
+        </div>
+      </div>
+
+      <div className="container page">
+        <div className="row article-content">
+          <div className="col-md-12">
+            <div>
+              <p>{body}</p>
+            </div>
+            <TagList tags={tagList} type="outline" />
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="article-actions">
+          <ArticleMeta article={article} />
+        </div>
+
+        <div className="row">
+          <div className="col-xs-12 col-md-8 offset-md-2">
+            <CommentForm slug={slug} />
+            <CommentList slug={slug} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ArticlePage;
