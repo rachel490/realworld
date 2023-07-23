88163194a6f12e1cd7b94716cae8c0987fce85e7
rachel490 <ccori12@naver.com>
2023-07-23 18:58:21 +0900

    :sparkles: Add auth to axios interceptor

--- api/axios.ts
+++ api/axios.ts
@@ -1,17 +1,37 @@
-import axios, { AxiosError } from "axios";
+/* eslint-disable no-param-reassign */
+import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
+import { PAGE_LINKS } from "@/constants/links";
+import { redirectPage } from "@/utils/redirect";
+import { getTokenCookie } from "@/utils/token";
 
 export const realWorldApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_KEY,
   headers: {
     "Content-Type": "application/json",
   },
+  params: {
+    isAuth: false,
+  },
   timeout: 5000,
 });
 
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  params?: {
+    isAuth: boolean;
+  };
+}
+
 realWorldApi.interceptors.request.use(
-  config => {
-    const componentType = typeof window === "undefined" ? "server" : "client";
-    console.log("😘 server or client : ", componentType);
+  async (config: CustomAxiosRequestConfig) => {
+    const { params } = config;
+    if (params && params.isAuth) {
+      const token = await getTokenCookie();
+      if (!token) {
+        redirectPage(PAGE_LINKS.register);
+      }
+
+      config.headers.Authorization = `Bearer ${token as string}`;
+    }
     return config;
   },
   error => Promise.reject(error),
@@ -21,13 +41,18 @@ realWorldApi.interceptors.response.use(
   response => {
     return response;
   },
-  (error: AxiosError) => {
+  async (error: AxiosError) => {
     const { response } = error;
 
     switch (response?.status) {
-      case 401:
-        console.log("🚨 401 error 🚨");
+      case 401: {
+        const token = await getTokenCookie();
+        if (!token) {
+          redirectPage(PAGE_LINKS.register);
+          console.log("🚨 401 error 🚨 : No Auth");
+        }
         break;
+      }
       case 403:
         console.log("🚨 403 error 🚨");
         break;
