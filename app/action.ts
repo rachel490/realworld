ba3ff82bbfdd979d6ab6bd9ccc0fdf1cf38f8e0e
rachel490 <ccori12@naver.com>
2023-07-23 18:26:35 +0900

    :sparkles: Declare cookie related server action

---
+++ app/action.ts
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/require-await */
+
+"use server";
+
+import { cookies } from "next/headers";
+import { JWTKey } from "@/constants/cookie";
+
+export async function setToken(token: string) {
+  cookies().set(JWTKey, token);
+}
+
+export async function deleteToken() {
+  cookies().set(JWTKey, "");
+}
+
+export async function getToken() {
+  const token = cookies().get(JWTKey)?.value;
+  return token;
+}
+
+export async function checkToken() {
+  cookies().has(JWTKey);
+}
---
+++ constants/cookie.ts
@@ -0,0 +1,1 @@
+export const JWTKey = "jwt";
