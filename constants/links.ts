108d1cad5d729f52764e4138a8a84be9ce1c21fd
rachel490 <ccori12@naver.com>
2023-07-25 18:58:03 +0900

    :bug: Modify page links to prevent multiple encode
    rename params to ensure if it's encoded or not

--- constants/links.ts
+++ constants/links.ts
@@ -3,8 +3,10 @@ export const PAGE_LINKS = {
   register: "/register",
   login: "/login",
   article: (slug: string) => `/article/${slug}`,
-  profilePosts: (username: string) => `/profile/${encodeURIComponent(username)}`,
-  profileFavorites: (username: string) => `/profile/${encodeURIComponent(username)}/favorites`,
+  profilePosts: (username: string, isEncoded = false) => {
+    return `/profile/${isEncoded ? username : encodeURIComponent(username)}`;
+  },
+  profileFavorites: (encodedUsername: string) => `/profile/${encodedUsername}/favorites`,
   newPost: "/editor",
   editPost: (slug: string) => `/editor/${slug}`,
   settings: "/settings",
