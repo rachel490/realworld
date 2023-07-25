9b604b8dda39df5acb05a1a427e7932dda15e89f
rachel490 <ccori12@naver.com>
2023-07-25 20:07:54 +0900

    :recycle: Change links in feed navbar based on current tab

--- components/FeedNavbar/FeedNavbar.tsx
+++ components/FeedNavbar/FeedNavbar.tsx
@@ -1,12 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Link from "next/link";
 import { PAGE_LINKS } from "@/constants/links";
+import { getServerComponentPathname } from "@/utils/serverActions";
 
 interface IProps {
   feedList: string[];
   currentFeed: string;
 }
 
+const feedLinks: { [key: string]: string } = {
+  "Global Feed": PAGE_LINKS.home,
+  "Your Feed": `${PAGE_LINKS.home}?feed=my`,
+};
+
 function FeedNavbar({ feedList, currentFeed }: IProps) {
+  const pathname = getServerComponentPathname();
+
   return (
     <div className="feed-toggle">
       <ul className="nav nav-pills outline-active">
@@ -14,7 +23,11 @@ function FeedNavbar({ feedList, currentFeed }: IProps) {
           <li className="nav-item" key={feedName}>
             <Link
               className={`nav-link ${currentFeed === feedName ? "active" : ""}`}
-              href={PAGE_LINKS.home}
+              href={
+                feedName.includes("Feed")
+                  ? feedLinks[feedName]
+                  : `${PAGE_LINKS.home}?${pathname.split("?")[1]}`
+              }
             >
               {feedName}
             </Link>
