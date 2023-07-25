9824bef4933e5066f0da8afe55a66be9c2bbcfd6
rachel490 <ccori12@naver.com>
2023-07-25 21:29:03 +0900

    :sparkles: Add middleware for protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { JWTKey } from "./constants/cookie";
 
-export function middleware(request: Request) {
+export function middleware(request: NextRequest) {
+  const isLoggedIn = request.cookies.has(JWTKey);
+
+  if (
+    (request.nextUrl.pathname.includes("/settings") ||
+      request.nextUrl.pathname.includes("/editor")) &&
+    !isLoggedIn
+  ) {
+    return NextResponse.redirect(new URL("/register", request.url));
+  }
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-pathUrl", request.url);
 
