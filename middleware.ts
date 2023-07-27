218245b4c0b5131d494219f89b03b3a1433ca503
rachel490 <ccori12@naver.com>
2023-07-27 15:02:15 +0900

    :recycle: Prevenet session check for every route in middleware

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,29 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
-
   if (
-    (request.nextUrl.pathname.includes("/settings") ||
-      request.nextUrl.pathname.includes("/editor")) &&
-    !session
+    request.nextUrl.pathname.includes("/settings") ||
+    request.nextUrl.pathname.includes("/editor") ||
+    request.nextUrl.pathname.includes("/login") ||
+    request.nextUrl.pathname.includes("/register")
   ) {
-    return NextResponse.redirect(new URL("/register", request.url));
-  }
+    const session = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
 
-  if (
-    (request.nextUrl.pathname.includes("/login") ||
-      request.nextUrl.pathname.includes("/register")) &&
-    session
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
+    if (
+      (request.nextUrl.pathname.includes("/settings") ||
+        request.nextUrl.pathname.includes("/editor")) &&
+      !session
+    ) {
+      return NextResponse.redirect(new URL("/register", request.url));
+    }
+
+    if (
+      (request.nextUrl.pathname.includes("/login") ||
+        request.nextUrl.pathname.includes("/register")) &&
+      session
+    ) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 
   const requestHeaders = new Headers(request.headers);
