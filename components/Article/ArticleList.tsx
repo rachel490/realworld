88d44b443e72093afd605aaa523bb04dba4a5161
rachel490 <ccori12@naver.com>
2023-07-28 18:34:42 +0900

    :recycle: Move data fetch logic & pagination inside ArticleList
    type prop for different data fetching api

--- components/Article/ArticleList.tsx
+++ components/Article/ArticleList.tsx
@@ -1,23 +1,60 @@
-import { IArticle } from "@/types";
+import { articleApi } from "@/api/domain/article";
+import { IMainPageProps } from "@/app/page";
+import { getServerComponentPathname } from "@/utils/serverActions";
 import ArticleCard from "./ArticleCard";
+import Pagination from "../Home/Pagination";
 
-interface IProps {
-  articlesData: IArticle[];
-}
+type IProps = {
+  type: "global feed" | "my feed" | "user post" | "user liked";
+  searchParams?: IMainPageProps["searchParams"];
+  params?: {
+    username: string;
+  };
+};
 
-function ArticleList({ articlesData }: IProps) {
+const LIMIT = 10;
+
+const getArticlesData = (
+  type: IProps["type"],
+  searchParams?: IProps["searchParams"],
+  params?: IProps["params"],
+) => {
+  switch (type) {
+    case "global feed":
+      return articleApi.getGlobalFeed(searchParams?.tagName, searchParams?.page);
+    case "my feed":
+      return articleApi.getMyFeed(searchParams?.page);
+    case "user post":
+      return articleApi.getUserPostedArticles(params?.username as string);
+    case "user liked":
+      return articleApi.getUserFavoritedArticles(params?.username as string);
+    default:
+      return { articles: [], articlesCount: 0 };
+  }
+};
+
+async function ArticleList({ searchParams, type, params }: IProps) {
+  const { articles, articlesCount } = await getArticlesData(type, searchParams, params);
+
+  const totalPages = Math.ceil(articlesCount / LIMIT);
+
   return (
-    <div>
-      {articlesData.length ? (
-        <>
-          {articlesData.map(article => (
-            <ArticleCard articleData={article} key={article.slug} />
-          ))}
-        </>
-      ) : (
-        <div className="article-preview">No articles are here... yet.</div>
+    <>
+      <div>
+        {articles.length ? (
+          articles.map(article => <ArticleCard articleData={article} key={article.slug} />)
+        ) : (
+          <div className="article-preview">No articles are here... yet.</div>
+        )}
+      </div>
+      {["my feed", "global feed"].includes(type) && (
+        <Pagination
+          totalPages={totalPages}
+          searchParams={searchParams}
+          currentURL={getServerComponentPathname()}
+        />
       )}
-    </div>
+    </>
   );
 }
 
